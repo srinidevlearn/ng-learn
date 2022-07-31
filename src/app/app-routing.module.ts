89c72d40005e5778c7653a4788fe23e8843a7b57
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-07-31 10:57:45 +0530

    added routing and lazyloading concept
    added routing and lazyloading concept

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'to-do', component: TodoComponent },
+  { path: 'profile', component: ProfileComponent },
+  {
+    path: 'shopping',
+    loadChildren: () =>
+      import('./shopping/shopping.module').then((m) => m.ShoppingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,18 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { LoginComponent } from './components/login/login.component';
 import { ApiService } from './service/api.service';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, LoginComponent],
+  declarations: [AppComponent, TodoComponent, LoginComponent, ProductCardComponent,ProfileComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    
+    CommonModule    
   ],
   providers: [ApiService],
   bootstrap: [AppComponent],
